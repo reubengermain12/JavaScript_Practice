e5a07f401ee3079d4f9f531c7bf3b0a614128f31
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-01 12:16:41 +0000

    Added a comment on how to use comments in JavaScript

--- TNN/test.js
+++ TNN/test.js
@@ -1,1 +1,8 @@
 alert("Hello World!:");
+
+//This is how JavaScript comments a line (by adding two backlash symbols at the beginning of the line).
+
+/* This is how to comment out a section, by wrapping a backslash & star symbol at the beginning around the content, and adding a star & backslash symbol at the end of the section.
+
+alert("Lets all live in peace & harmony:");
+*/
