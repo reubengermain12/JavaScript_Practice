ef9bbfca955c006ebe1d1c58fcb4e4f6458fff75
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-01 12:19:18 +0000

    1st & second alert comment example

--- TNN/test.js
+++ TNN/test.js
@@ -1,8 +1,5 @@
+//the first alert
 alert("Hello World!:");
 
-//This is how JavaScript comments a line (by adding two backlash symbols at the beginning of the line).
-
-/* This is how to comment out a section, by wrapping a backslash & star symbol at the beginning around the content, and adding a star & backslash symbol at the end of the section.
-
+//the second alert
 alert("Lets all live in peace & harmony:");
-*/
