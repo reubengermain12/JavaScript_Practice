b51d2682d28bbd742e15899f95c320e28b9660b2
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-01 13:10:51 +0000

    Added some comments about Variables

--- TNN/test.js
+++ TNN/test.js
@@ -1,6 +1,15 @@
+//This is stored in 2 lines of code
 var myVariableReuben;
 myVariableReuben = 10;
 
-
+//This is stored in 1 line of code
 var myVar2 = "hello";
-myVar2
+myVar2;
+
+var customerAge = 32;
+customerAge;
+
+var customerName = "Reuben";
+customerName;
+
+//JavaScript is a weakly typed language, meaning you don't have to specify the type like in other languages.
