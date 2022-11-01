bdb14a854f86f52dfcbfecb9890ceadb176ae386
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-01 13:03:16 +0000

    These are 2 ways of writing variables

--- TNN/test.js
+++ TNN/test.js
@@ -1,8 +1,6 @@
-//Summary
+var myVariableReuben;
+myVariableReuben = 10;
 
-// - JavaScript is case sensitive
-// - Contains many statements, all ending with a semicolon;
-// - Not sensitive to whitespace or line breaks
-// - Write one-line comment using //
-// - Write multi line comments using /* */
-// - JavaScript runs from top to bottom
+
+var myVar2 = "hello";
+myVar2
