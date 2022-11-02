bbfe60a5ec3a8aa09ec24218d387e82bd4937f33
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-02 11:19:38 +0000

    Added another comment with some code changes

--- TNN/test.js
+++ TNN/test.js
@@ -1,10 +1,10 @@
 var youLikeMeat = true;
-var myNum = 8
+var myNum = 10
+//I have changed the line aboves value from 8 to 10.
+//Now the if variable will change it's output because it is true
 
 if (myNum == 10) {
-//The line above is saying if this is true, go ahead and excecute the line below.
- 
-//If it is not true go to the else line and excecute that code instead.
+
   document.write("myNum is equal to 10");
 
 } else{
