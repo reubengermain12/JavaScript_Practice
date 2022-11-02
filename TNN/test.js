5e61799dcd38fa39b6bc77ed6650e8d1ad4e8ebc
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-02 10:58:11 +0000

    Added some comments

--- TNN/test.js
+++ TNN/test.js
@@ -1,6 +1,10 @@
 var youLikeMeat = true;
+// I've assigned the above variable the value of true
 
 if (youLikeMeat) {
+// Then i'm checking the variable to see if it is true
 
   document.write("Here is the meaty menu...");
+  // If it's true then it will excecute this block of code
+  // If it's not true then if will not excecute
 }
